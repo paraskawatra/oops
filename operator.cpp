d73986862cc7f7786aa72803674d53279f3e4719
paraskawatra <101243260+paraskawatra@users.noreply.github.com>
2022-05-18 10:14:43 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class person
+{
+ public:
+ bool a;
+ person(bool a=0){
+    this-> a=a;
+ }
+ void display(){
+     cout<<"value of a "<<a;
+ }
+ void operator!(){
+     a=~a;
+ }
+};
+int main(){
+    int n;
+    cin>>n;
+    person obj(n);
+    !obj;
+    obj.display();
+}
